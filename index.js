7b6b0444a9d13ffb43b23be9943eb845f4be4b27
Keenan Sturtevant <keenan.sturtevant@childrens.com>
2018-10-09 20:54:23 -0500

    add yellowbox warning skip

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ import { YellowBox } from 'react-native'
 
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader'])
 YellowBox.ignoreWarnings(['Class RCTCxxModule'])
+YellowBox.ignoreWarnings(['Warning: componentWillReceiveProps is deprecated'])
 
 AppRegistry.registerComponent('BaldHeadIslandApp', () => App)
